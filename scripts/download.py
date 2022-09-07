2c8aa5fd05c7434c4343eb7eba7f99069ec65f97
evavaa <evach0305@gmail.com>
2022-09-07 23:17:26 +1000

    optimize postcode processing and observe missing values

--- scripts/download.py
+++ scripts/download.py
@@ -21,15 +21,16 @@ def download_external_data():
     req = Request(url=url_all_postcodes, headers={'User-Agent': 'Mozilla/5.0'})
     with open(external_output_path + "australian_postcodes.csv", "wb") as f:
         f.write(urlopen(req).read())
-    print("Finished!")
+    print("Completed!")
 
     print("Downloading SA2 shapefile......")
     zip_path, _ = urlretrieve(url_SA2_shapefile)
     with zipfile.ZipFile(zip_path, "r") as f:
         f.extractall(external_output_path + "SA2_2021")
-    print("Finished!")
+    print("Completed!")
 
     print("Downloading total income......") 
     urlretrieve(url_total_income, external_output_path+"total_income.xlsx")
+    print("Completed!")
 
 download_external_data()
