8239af3bdf8d68b87ae2bc1aba0879575b63c397
evavaa <evach0305@gmail.com>
2022-10-06 12:31:31 +1100

    fraud model script

--- scripts/fraud_model.py
+++ scripts/fraud_model.py
@@ -1,13 +1,11 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-import pandas as pd
 from pyspark.ml.feature import StringIndexer
 from pyspark.ml import Pipeline 
 from pyspark.ml.feature import VectorAssembler 
 from pyspark.ml.feature import OneHotEncoder
 from pyspark.sql.functions import rand
 from pyspark.ml.classification import LogisticRegression
-from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
 # Create a spark session (which will run spark jobs)
 spark = (
@@ -20,12 +18,7 @@ spark = (
     .getOrCreate()
 )
 
-def preprocess():
-    # read in merged dataset and delta files
-    full = spark.read.parquet("../../data/curated/full_data/")
-    probs_merchant = spark.read.option('header', True).csv('../../data/tables/merchant_fraud_probability.csv')
-    probs_consumer= spark.read.option('header', True).csv('../../data/tables/consumer_fraud_probability.csv')   
-
+def preprocess(full, probs_merchant, probs_consumer): 
     # convert features to appropriate data types
     probs_consumer =  probs_consumer.withColumn('user_id', F.col('user_id').cast('long'))\
         .withColumn('fraud_probability', F.col('fraud_probability').cast('float'))
@@ -45,7 +38,7 @@ def preprocess():
     # set benchmark as 5% to focus on False Positive instead of False Negative
     full = full.withColumn('is_fraud', F.when((F.col('merchant_prob') > 5) | (F.col('consumer_prob') > 5), 1).otherwise(0))
 
-    # TODO: what does this line do?
+    # discard extremely small values
     full = full.filter(F.col('dollar_value') >= 1).na.drop(subset = 'name')
     full =  full.withColumn('month', F.month('order_datetime'))
 
@@ -57,29 +50,29 @@ def feature_engineering(full):
     indexed_features = ['revenue_level', 'tags', 'gender']
     indexers =[]
     for col in indexed_features:
-    indexers.append(StringIndexer(inputCol=col, outputCol = col+"_index"))
+        indexers.append(StringIndexer(inputCol=col, outputCol = col+"_index"))
 
     # one-hot encode the numeric indices
     categorical_features =  ["tags_index", "gender_index","month"]
     ohe = []
     for f in categorical_features:
-    ohe.append(OneHotEncoder(inputCol=f, outputCol=f+"OHE"))
-    pipeline = Pipeline(stages=[indexers, ohe])
-    indexed_result = pipeline.fit(full).transform(full)
+        ohe.append(OneHotEncoder(inputCol=f, outputCol=f+"OHE"))
+    pipeline = Pipeline(stages=indexers + ohe)
+    processed_data = pipeline.fit(full).transform(full)
 
     # feature selection and vectorisation
     feature_selected = ['dollar_value','take_rate','mean_total_income','monthOHE','revenue_level_index','tags_indexOHE','gender_indexOHE']
     assembler = VectorAssembler(inputCols=feature_selected ,outputCol='features')
-    output = assembler.transform(indexed_result)
+    processed_data = assembler.transform(processed_data)
 
-    return output
+    return processed_data
 
 
-def model(output):
-    train_data = output.filter(F.col('order_datetime') < '2022-02-28')
-    predict_data = output.filter(F.col('order_datetime') >= '2022-02-28')   
+def model(processed_data):
+    train_data = processed_data.filter(F.col('order_datetime') < '2022-02-28')
+    predict_data = processed_data.filter(F.col('order_datetime') >= '2022-02-28')   
 
-    # balance distribution of two classes
+    # balance distribution of two classes and shuffle the dataset randomly
     fraud_data = train_data.filter(F.col('is_fraud') == 1)
     normal_data = train_data.filter(F.col('is_fraud') == 0).randomSplit([0.01,0.99])[0]
     train_data = fraud_data.union(normal_data).orderBy(rand())
@@ -88,13 +81,36 @@ def model(output):
     lr = LogisticRegression(labelCol='is_fraud')
     fitted_model = lr.fit(train_data)
     fitted_model.setFeaturesCol("features")
-    fitted_model.setPredictionCol("prediction")
+    fitted_model.setPredictionCol('is_fraud')
 
     predict_data = predict_data.drop("is_fraud")
     predicted = fitted_model.transform(predict_data)
 
-    cols = ['user_id','order_datetime','merchant_abn','SA2_code','postcode','consumer_id','state','gender','mean_total_income','dollar_value','order_id','name','tags','revenue_level','take_rate','is_fraud'	]
+    cols = ['user_id','order_datetime','merchant_abn','SA2_code','postcode','consumer_id','state',
+    'gender','mean_total_income','dollar_value','order_id','name','tags','revenue_level','take_rate','is_fraud']
     predicted =  predicted.select(cols)
-    ???
 
+    return predicted
+    
+
+def main():
+    # read in merged dataset and delta files
+    full = spark.read.parquet("../data/curated/full_data/")
+    probs_merchant = spark.read.option('header', True).csv('../data/tables/merchant_fraud_probability.csv')
+    probs_consumer= spark.read.option('header', True).csv('../data/tables/consumer_fraud_probability.csv') 
+
+    full = preprocess(full, probs_merchant, probs_consumer)
+    processed_data = feature_engineering(full)
+    predicted = model(processed_data)
+
+    # combine training data and predicted data
+    full = full.drop('merchant_prob', 'consumer_prob', 'month')
+    full = full.filter(F.col('order_datetime') < '2022-02-28')
+    full = full.union(predicted)
+    full = full.withColumn("is_fraud", F.col("is_fraud").cast("INT"))
+
+    full.write.format('parquet').mode('overwrite').save("../data/curated/full_data_without_fraud")
+
+
+main()
 
